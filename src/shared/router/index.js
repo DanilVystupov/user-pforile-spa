5d8a974b95d81dc0919c12689fd061c808b1db71
DanilVystupov <davaimne58@gmail.com>
2023-12-13 01:17:56 +0300

    add: added user details page and page with posts user

--- src/shared/router/index.js
+++ src/shared/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import MainPage from '@/pages/MainPage.vue';
+import UserDetailsPage from '@/pages/UserDetailsPage.vue';
+import UserPostsPage from '@/pages/UserPostsPage.vue';
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'main',
     component: MainPage
+  },
+  {
+    path: '/user/:id', 
+    name: 'userDetails',
+    component: UserDetailsPage,
+  },
+  {
+    path: '/user/:id/posts', 
+    name: 'userPosts',
+    component: UserPostsPage,
   },
 ]
 
